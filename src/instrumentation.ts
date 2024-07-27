e0c4a87a1035503e3e6a850b61f39779497f3b28
Fallagar <fallagar2010@gmail.com>
2024-07-28 00:19:55 +0300

    cron setup

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -5,12 +5,9 @@ const timeout = UPDATE_DB_INTERVAL || 300000;
 export function register() {
     setInterval(async () => {
         try {
-            console.log(1231);
-            // await axios.get(`${NEXT_SERVER_URL}/api/fetch-rss`);
-            // await axios.get(`/api/fetch-rss`);
-            // console.log("FETCH HAPPENED", `${NEXT_SERVER_URL}/api/fetch-rss`);
+            await axios.get(`${NEXT_SERVER_URL}/api/fetch-rss`);
         } catch (error: any) {
-            console.log(error);
+            console.log(error?.message);
         }
     }, Number(timeout));
 }
