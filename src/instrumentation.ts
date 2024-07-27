7fc01210574cae1c5a6a0e2834725e4177c50f69
Fallagar <fallagar2010@gmail.com>
2024-07-28 00:31:50 +0300

    remove log

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -5,7 +5,6 @@ const timeout = UPDATE_DB_INTERVAL || 300000;
 export function register() {
     setInterval(async () => {
         try {
-            console.log(123);
             await axios.get(`${NEXT_SERVER_URL}/api/fetch-rss`);
         } catch (error: any) {
             console.log(error?.message);
