b4072eacc065d30dea74f56ef8c3ff8581e20570
Fallagar <fallagar2010@gmail.com>
2024-07-28 00:23:31 +0300

    Update route.ts

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -66,12 +66,6 @@ function contentParser(item: IFeedItem) {
     }
 }
 export async function GET(request: NextRequest) {
-    const authHeader = request.headers.get("authorization");
-    if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
-        return new Response("Unauthorized", {
-            status: 401,
-        });
-    }
     await connectDB();
     const feed = await parser.parseURL("http://www.reddit.com/.rss?limit=20");
 
