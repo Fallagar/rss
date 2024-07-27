2da35ad6321fa2a94ed5913e03856b282f09a986
Fallagar <fallagar2010@gmail.com>
2024-07-27 23:24:27 +0300

    Update instrumentation.ts

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -5,10 +5,11 @@ const timeout = UPDATE_DB_INTERVAL || 300000;
 export function register() {
     setInterval(async () => {
         try {
-            await axios.get(`${NEXT_SERVER_URL}/api/fetch-rss`);
-            console.log("FETCH HAPPENED", `${NEXT_SERVER_URL}/api/fetch-rss`);
+            // await axios.get(`${NEXT_SERVER_URL}/api/fetch-rss`);
+            await axios.get(`/api/fetch-rss`);
+            // console.log("FETCH HAPPENED", `${NEXT_SERVER_URL}/api/fetch-rss`);
         } catch (error: any) {
-            console.log(error?.message);
+            console.log(error);
         }
     }, Number(timeout));
 }
