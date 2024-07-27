ed5f51f3d30ea327ae3cc5999b04a43e7caf1bc4
Fallagar <fallagar2010@gmail.com>
2024-07-28 00:10:44 +0300

    vercel trigger

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -5,7 +5,7 @@ const timeout = UPDATE_DB_INTERVAL || 300000;
 export function register() {
     setInterval(async () => {
         try {
-            console.log(123);
+            console.log(1231);
             // await axios.get(`${NEXT_SERVER_URL}/api/fetch-rss`);
             // await axios.get(`/api/fetch-rss`);
             // console.log("FETCH HAPPENED", `${NEXT_SERVER_URL}/api/fetch-rss`);
